9024f94f2a72f093180c4ad053de8e13aaff184f
Rafael Lima <Rafael-lima-1997@hotmail.com>
2014-05-03 16:49:15 -0300

    Update ng-ga.js

--- src/ng-ga.js
+++ src/ng-ga.js
@@ -4,36 +4,33 @@
  * License: MIT
  */
 
-;(function (angular, undefined) {
-	'use strict';
+'use strict';
 
-	angular.module('ngGA', [])
-		.directive('ngGoogleAnalytics', [function () {
-			return {
-				restrict: 'EA',
-				scope: {
-					domain: '@',
-					code: '@'
-				},
-				link: function (scope, element, attrs) {
+angular.module('ngGA', [])
+	.directive('ngGoogleAnalytics', [function () {
+		return {
+			restrict: 'EA',
+			scope: {
+				domain: '@',
+				code: '@'
+			},
+			link: function (scope, element, attrs) {
 
-					scope.domain = scope.domain || 'default.com';
-					scope.code = scope.code || 'UA-XXXX-X';
-					
-					var gaScript = (function () {
+				scope.domain = scope.domain || 'default.com';
+				scope.code = scope.code || 'UA-XXXX-X';
+				
+				var gaScript = (function () {
 
-						(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-						(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-						m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-						})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
-					 
-						ga('create', scope.code, scope.domain);
-						ga('send', 'pageview');
+					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+					})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+				 
+					ga('create', scope.code, scope.domain);
+					ga('send', 'pageview');
 
-					}());
+				}());
 
-				}
-			};
-		}]);
-
-}(window.angular));
+			}
+		};
+	}]);
